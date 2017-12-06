55c87475c5c2414f39311136f6e57635e78d28e9
Nick <nikolay@incuber.com>
2017-12-06 23:52:36 +0200

    start dnd use

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import './index.css';
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Board from './Board';
+import { observe } from './Game';
+
+const rootEl = document.getElementById('root');
+
+observe(knightPosition =>
+    ReactDOM.render(
+        <Board knightPosition={knightPosition} />,
+        rootEl
+    )
+);
